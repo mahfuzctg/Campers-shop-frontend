6768d5b5594f0ac2b93af0e9b7c0c1c470bf85e9
Mahfuz <aamahfuz.pro@gmail.com>
2024-07-10 07:14:27 +0600

    basic setup done!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to , App Page!</h1>
+      <Button>click here</Button>
+    </div>
+  );
+};
+
+export default App;
